658983ebe5aeedf901e19d9a17332c6c1c7d55b9
Sriram Sreedhar <sriramsreedhar003@gmail.com>
2019-01-09 19:33:13 +0530

    Create integer-and-float.py

---
+++ integer-and-float.py
@@ -0,0 +1,16 @@
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+a=int(input())
+b=int(input())
+
+print(a//b)
+print(a/(b*1.0))
+
+'''
+--output--
+srirams-mbp:HackerRank sriramsreedhar$ ./integer-float.py 
+10
+3
+3
+3.3333333333333335
+srirams-mbp:HackerRank sriramsreedhar$ 
+'''
