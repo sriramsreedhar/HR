84456d6bca292b6c1d6d51520bdd17bd3c059dd2
Sriram Sreedhar <sriramsreedhar003@gmail.com>
2019-01-09 16:04:28 +0530

    Create two-integers.py

---
+++ two-integers.py
@@ -0,0 +1,16 @@
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+'''
+Task 
+Read two integers from STDIN and print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+'''
+
+a=int(input())
+b=int(input())
+
+print(a+b)
+print(a-b)
+print(a*b)
